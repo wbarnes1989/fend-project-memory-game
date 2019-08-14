437e6472a602d82356018a5550859e25421d6feb
William Barnes <jakebarnes1989@gmail.com>
2019-08-14 18:10:14 -0500

    Update app.js
    removed spaces, added code for checking cards match and clearing when they don't.
    
    Still need to sort out locking open matched cards while allowing future cards to be checked.

--- js/app.js
+++ js/app.js
@@ -10,63 +10,63 @@ let deck = document.querySelector('.deck');
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
- // shuffle cards
- shuffle(cards);
- // Create card html
- for (let i = 0; i < cards.length; i++) {
+// shuffle cards
+shuffle(cards);
+// Create card html
+for (let i = 0; i < cards.length; i++) {
 
-   if (cards[i] === 'Diamond') {
-     cards[i] = '<li class="card">' +
-     '<i class="fa fa-diamond"></i>' +
-     '</li>';
-   }
+  if (cards[i] === 'Diamond') {
+    cards[i] = '<li class="card">' +
+    '<i class="fa fa-diamond"></i>' +
+    '</li>';
+  }
 
-   if (cards[i] === 'Plane') {
-      cards[i] = '<li class="card">' +
-      '<i class="fa fa-paper-plane-o"></i>' +
-      '</li>';
-   }
+  if (cards[i] === 'Plane') {
+    cards[i] = '<li class="card">' +
+    '<i class="fa fa-paper-plane-o"></i>' +
+    '</li>';
+  }
 
-   if (cards[i] === 'Anchor') {
-     cards[i] = '<li class="card">' +
-     '<i class="fa fa-anchor"></i>' +
-     '</li>';
-   }
+  if (cards[i] === 'Anchor') {
+    cards[i] = '<li class="card">' +
+    '<i class="fa fa-anchor"></i>' +
+    '</li>';
+  }
 
-   if (cards[i] === 'Bolt') {
-     cards[i] = '<li class="card">' +
-     '<i class="fa fa-bolt"></i>' +
-     '</li>';
-   }
+  if (cards[i] === 'Bolt') {
+    cards[i] = '<li class="card">' +
+    '<i class="fa fa-bolt"></i>' +
+    '</li>';
+  }
 
-   if (cards[i] === 'Cube') {
-     cards[i] = '<li class="card">' +
-     '<i class="fa fa-cube"></i>' +
-     '</li>';
-   }
+  if (cards[i] === 'Cube') {
+    cards[i] = '<li class="card">' +
+    '<i class="fa fa-cube"></i>' +
+    '</li>';
+  }
 
-   if (cards[i] === 'Leaf') {
-     cards[i] = '<li class="card">' +
-     '<i class="fa fa-leaf"></i>' +
-     '</li>';
-   }
+  if (cards[i] === 'Leaf') {
+    cards[i] = '<li class="card">' +
+    '<i class="fa fa-leaf"></i>' +
+    '</li>';
+  }
 
-   if (cards[i] === 'Bicycle') {
-     cards[i] = '<li class="card">' +
-     '<i class="fa fa-bicycle"></i>' +
-     '</li>';
-   }
+  if (cards[i] === 'Bicycle') {
+    cards[i] = '<li class="card">' +
+    '<i class="fa fa-bicycle"></i>' +
+    '</li>';
+  }
 
-   if (cards[i] === 'Bomb') {
-     cards[i] = '<li class="card">' +
-     '<i class="fa fa-bomb"></i>' +
-     '</li>';
-   }
- }
- // Append HTML to deck
- for (let i = 0; i < cards.length; i++) {
-   deck.innerHTML += cards[i];
- }
+  if (cards[i] === 'Bomb') {
+    cards[i] = '<li class="card">' +
+    '<i class="fa fa-bomb"></i>' +
+    '</li>';
+  }
+}
+// Append HTML to deck
+for (let i = 0; i < cards.length; i++) {
+  deck.innerHTML += cards[i];
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -94,3 +94,45 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+let openCards = [];
+
+function cardClicked(clickEvent) {
+  clickEvent.target.classList.add('match');
+  openCards.push(clickEvent.target);
+
+  if (openCards !== undefined || openCards.length !== 0) {
+    if (openCards.length > 1) {
+      console.log('there is more than one card selected');
+      //cardMatch(openCards);
+      cardsMatch = openCards[0].innerHTML == openCards[1].innerHTML;
+      if (cardsMatch) {
+        console.log('cards matched!');
+      }
+
+      if (!cardsMatch) {
+        console.log('cards did not match');
+        for (let i = 0; i < openCards.length; i++) {
+          openCards[i].classList.remove('match');
+        }
+        openCards.pop();
+        openCards.pop();
+      }
+
+    }
+  }
+}
+
+function cardMatch() {
+
+}
+
+function cardsNotMatch() {
+
+}
+
+function addMove() {
+
+}
+
+deck.addEventListener('click', cardClicked);
